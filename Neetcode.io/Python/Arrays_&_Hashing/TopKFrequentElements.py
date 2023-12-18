78e160caf5f76298a981a2e56010b811275c0b7e
Vitor Cavalcante <101594191+vitorjpc10@users.noreply.github.com>
2023-12-18 07:58:14 -0300

    Updated syntax and documentation
    Updated syntax and documentation for TopKFrequencyElements

--- Neetcode.io/Python/Arrays_&_Hashing/TopKFrequentElements.py
+++ Neetcode.io/Python/Arrays_&_Hashing/TopKFrequentElements.py
@@ -39,6 +39,20 @@ class Solution(object):
         :param k: int - The number of top frequent elements to be returned.
         :return: List[int] - The list of top k frequent elements.
 
+        Time Complexity: O(n log n)
+        - The method iterates over the input list `nums` once to count the frequency of each number, which takes O(n) time, where n is the length of the list.
+        - Sorting the dictionary items based on their counts using the `sorted` function takes O(n log n) time complexity.
+        - Extracting the first k elements from the sorted list also takes O(k) time.
+        - Overall, the dominant factor in the time complexity is the sorting step, which has a time complexity of O(n log n).
+        - As the input size increases, the time taken by this method grows logarithmically.
+
+        Space Complexity: O(n)
+        - The method uses a dictionary, `num_count_dict`, to store the count of each number in `nums`.
+        - The space required for the dictionary is proportional to the number of unique elements in `nums`, which can be at most n.
+        - Sorting the dictionary items and creating the `result` list also require additional space proportional to the number of unique elements.
+        - Therefore, the space complexity is O(n), where n is the number of unique elements in `nums`.
+        - As the input size increases, the space taken by this method grows linearly.
+
         Algorithm:
         1. Create a dictionary, num_count_dict, to store the count of each number in nums.
         2. Iterate over each number in nums:
@@ -63,11 +77,11 @@ class Solution(object):
                 num_count_dict[num] = 1
 
         # Step 3: Sort the dictionary items based on their counts in descending order
-        sorted_num_count_dict = sorted(num_count_dict.items(), key=lambda x: x[1], reverse=True)
+        sorted_num_count_list = sorted(num_count_dict.items(), key=lambda x: x[1], reverse=True)
 
         # Step 4: Extract the first k elements from the sorted list
         result = []
-        for num_count in sorted_num_count_dict[0:k]:
+        for num_count in sorted_num_count_list[0:k]:
             result.append(num_count[0])
 
         # Step 7: Return the list of top k frequent elements
--- Neetcode.io/readme.md
+++ Neetcode.io/readme.md
@@ -1,6 +1,7 @@
 # Personal Notes
 
 ### Topics to review
-1. `Monotonic Stack`
-2. `Two pointer`
-3. `e.g.`
+1. Go over this syntax `sorted_num_count_list = sorted(num_count_dict.items(), key=lambda x: x[1], reverse=True)` for TopKFrequentElements problem
+2. `Monotonic Stack`
+3. `Two pointer`
+4. `e.g.`
